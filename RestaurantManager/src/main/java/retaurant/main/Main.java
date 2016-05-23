0431372cbf4d172c31a6ba37335f89acd95f1a2f
Beniamin <Beniamin@Benjamin>
2016-05-23 17:04:13 +0300

    View employees
    ...

--- RestaurantManager/src/main/java/retaurant/main/Main.java
+++ RestaurantManager/src/main/java/retaurant/main/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		AnchorPane root = new AnchorPane();
-		root = FXMLLoader.load(getClass().getResource("/restaurant/view/ClientActions.fxml"));
+		root = FXMLLoader.load(getClass().getResource("/restaurant/view/manager/ClientActions.fxml"));
 		Scene scene = new Scene(root, 900, 700);
 
 		primaryStage.setTitle("Manager App");
