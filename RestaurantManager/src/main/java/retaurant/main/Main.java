8f8628c208f763488ce2218fea418a9832b4ed45
Beniamin <Beniamin@Benjamin>
2016-05-23 11:21:36 +0300

    Finalize First View form Manager

---
+++ RestaurantManager/src/main/java/retaurant/main/Main.java
@@ -0,0 +1,35 @@
+package retaurant.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author Beniamin
+ * @since 2016-05-23 10:28 a.m
+ */
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		AnchorPane root = new AnchorPane();
+		root = FXMLLoader.load(getClass().getResource("/restaurant/view/Manager.fxml"));
+		Scene scene = new Scene(root, 900, 700);
+
+		primaryStage.setTitle("Manager App");
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/manager_icon.png")));
+		primaryStage.setOpacity(1.0);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
