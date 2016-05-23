30db0baddad398faae1880b38c6efb98324e2766
Florin <floriani@gmail.com>
2016-05-24 00:04:35 +0300

    employee

--- RestaurantManager/src/main/java/restaurant/datamapper/EmployeeMapper.java
+++ RestaurantManager/src/main/java/restaurant/datamapper/EmployeeMapper.java
@@ -9,8 +9,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.imageio.ImageIO;
 import com.mysql.jdbc.PreparedStatement;
+
+import restaurant.model.Employee;
+import restaurant.model.OrderDetail;
 
 
 /**
@@ -65,6 +70,90 @@ public class EmployeeMapper {
 		}
 
 		return null;
+	}
+	
+	public void insert(OrderDetail o) {
+		try {
+			String statement = "INSERT INTO order_details(order_id, product_id, product_name, quantity, status) VALUES (?,?,?,?,?)";
+			PreparedStatement dbStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(statement);
+			dbStatement.setInt(1, o.getOrder_id());
+			dbStatement.setInt(2, o.getProduct_id());
+			dbStatement.setString(3, o.getProduct_name());
+			dbStatement.setInt(4, o.getQuantity());
+			dbStatement.setString(5, o.getStatus());
+			dbStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Employee getNewestEmployee() {
+		Employee o = new Employee();
+		try {
+			String statement = "SELECT max(employee_no) FROM employee;";
+			PreparedStatement dbStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(statement);
+			ResultSet rs = dbStatement.executeQuery();
+			while (rs.next()) {
+				
+				int employee_no = rs.getInt("employee_no");
+				o.setEmployee_no(employee_no);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	public ArrayList<OrderDetail> findALL() {
+		ArrayList<OrderDetail> orders = new ArrayList<OrderDetail>();
+		try {
+			OrderDetail o;
+			String statement = "SELECT * FROM order_details";
+			PreparedStatement dbStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(statement);
+			ResultSet rs = dbStatement.executeQuery();
+			while (rs.next()) {
+				int order_no = rs.getInt("order_id");
+				int product_id = rs.getInt("product_id");
+				String product_name = rs.getString("product_name");
+				int quantity = rs.getInt("quantity");
+				String status = rs.getString("status");
+				o = new OrderDetail(order_no, product_id, product_name, quantity, status);
+				orders.add(o);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return orders;
+	}
+
+	public void update(OrderDetail o) {
+		try {
+			String statement = "UPDATE order_details SET product_id=?,product_name=?,quantity=?,status=? where order_id=? and product_id=?";
+			PreparedStatement dbStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(statement);
+			dbStatement.setInt(1, o.getProduct_id());
+			dbStatement.setString(2, o.getProduct_name());
+			dbStatement.setInt(3, o.getQuantity());
+			dbStatement.setString(4, o.getStatus());
+			dbStatement.setInt(5, o.getOrder_id());
+			dbStatement.setInt(6, o.getProduct_id());
+			dbStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void delete(OrderDetail o) {
+		try {
+			String statement = "DELETE FROM order_details where order_id=?";
+			PreparedStatement dbStatement = (PreparedStatement) DBConnection.getConnection().prepareStatement(statement);
+			dbStatement.setInt(1, o.getOrder_id());
+			dbStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	
