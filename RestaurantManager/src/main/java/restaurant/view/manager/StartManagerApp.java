599ffdcdaec08bf737f7c78952c5cabd6efa53d0
Florin <florinani@gmail.com>
2016-05-28 01:26:38 +0300

    comit

--- RestaurantManager/src/main/java/restaurant/view/manager/StartManagerApp.java
+++ RestaurantManager/src/main/java/restaurant/view/manager/StartManagerApp.java
@@ -3,9 +3,6 @@ package restaurant.view.manager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
